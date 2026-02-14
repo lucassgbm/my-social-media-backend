4a1fa24a0bd861afa966ac0e77703ac9c6b84c58
Lucas Galdino Belfort Matos <lgmatos@w3haus.local>
2026-02-14 17:56:46 -0300

    novo módulo de amigos

--- app/Models/UserFriend.php
+++ app/Models/UserFriend.php
@@ -8,9 +8,23 @@ class UserFriend extends Model
 {
     protected $table = 'user_friends';
 
+    public $timestamps = false;
+
     protected $fillable = [
         'user_id',
-        'user_id',
+        'friend_id',
+        'accepted',
+        'accepted_at'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function friend()
+    {
+        return $this->belongsTo(User::class, 'friend_id');
+    }
 
 }
